9a679f10fa17041c5529440e7dcb313afef7d944
exolop <80510724+Xlaaf@users.noreply.github.com>
2021-08-03 22:51:56 +0700

    Update __init__.py

--- acutebot/__init__.py
+++ acutebot/__init__.py
@@ -18,46 +18,18 @@ from functools import wraps
 from telegram.ext import Updater, Defaults
 from telegram import ChatAction, ParseMode
 
-ENV = bool(os.environ.get("ENV", False))
-if ENV:
-    TOKEN = os.environ.get("TOKEN")
-    WORKERS = int(os.environ.get("WORKERS", 8))
-    TMDBAPI = os.environ.get("TMDBAPI")
-    DB_URI = os.environ.get("DATABASE_URL")
-    GENIUS = os.environ.get("GENIUS")
-    SPT_CLIENT_SECRET = os.environ.get("SPT_CLIENT_SECRET")
-    SPT_CLIENT_ID = os.environ.get("SPT_CLIENT_ID")
-    DEBUG = bool(os.environ.get("DEBUG", False))
-    ARLTOKEN = os.environ.get("ARL")
-    APP_URL = os.environ.get("APP_URL")
-    APIID = os.environ.get("APIID")
-    APIHASH = os.environ.get("APIHASH")
+TOKEN = os.environ.get("TOKEN")
+TMDBAPI = os.environ.get("TMDBAPI")
+DB_URI = os.environ.get("DATABASE_URL")
+GENIUS = os.environ.get("GENIUS")
+SPT_CLIENT_SECRET = os.environ.get("SPT_CLIENT_SECRET")
+SPT_CLIENT_ID = os.environ.get("SPT_CLIENT_ID")
+ARLTOKEN = os.environ.get("ARL")
+APP_URL = os.environ.get("APP_URL")
+APIID = os.environ.get("APIID")
+APIHASH = os.environ.get("APIHASH")
 
-
-else:
-    from acutebot.config import Config
-
-    TOKEN = Config.TOKEN
-    WORKERS = Config.WORKERS
-    TMDBAPI = Config.TMDBAPI
-    DB_URI = Config.DB_URI
-    GENIUS = Config.GENIUS
-    SPT_CLIENT_SECRET = Config.SPT_CLIENT_SECRET
-    SPT_CLIENT_ID = Config.SPT_CLIENT_ID
-    DEBUG = Config.DEBUG
-    ARLTOKEN = Config.ARL
-    APP_URL = Config.APP_URL
-    APIID = Config.APIID
-    APIHASH = Config.APIHASH
-
-
-if DEBUG:
-    logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=logging.DEBUG,
-    )
-else:
-    logging.basicConfig(
+logging.basicConfig(
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         level=logging.INFO,
     )
@@ -88,5 +60,5 @@ def typing(func):
 
 # Use HTML treewide;
 defaults = Defaults(parse_mode=ParseMode.HTML)
-updater = Updater(TOKEN, use_context=True, workers=WORKERS, defaults=defaults)
+updater = Updater(TOKEN, use_context=True, workers=8, defaults=defaults)
 dp = updater.dispatcher
